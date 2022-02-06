8ab6303e91b4c155aad97f0e172b3a868e8e0914
tarooooooo <ryutaromaeda83@gmail.com>
2022-02-06 22:54:54 +0900

    管理者一覧ページ追加

--- app/controllers/admin/admin_users/sessions_controller.rb
+++ app/controllers/admin/admin_users/sessions_controller.rb
@@ -4,9 +4,9 @@ class Admin::AdminUsers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
@@ -14,11 +14,15 @@ class Admin::AdminUsers::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  def destroy
-    super
-  end
+  # def destroy
+  #   super
+  # end
 
-  # protected
+  private
+
+    def after_sign_in_path_for(resource)
+      admin_root_path(resource)
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
