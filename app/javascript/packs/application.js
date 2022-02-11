998e9ebce2af4febbd3ed4d3c168d7e9ad23130e
tarooooooo <ryutaromaeda83@gmail.com>
2022-02-11 18:00:02 +0900

    アコーディオン機能追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("test.js")
 require("packs/public/top_page/show.js")
 require("admin-lte");
 
