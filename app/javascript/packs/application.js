57894075e77c0af766fb203c64918df20684ef6c
tarooooooo <ryutaromaeda83@gmail.com>
2022-02-11 17:16:34 +0900

    トップページのFAQアコーディオン追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require('jquery_ujs')
+require("test.js")
+require("packs/public/top_page/show.js")
 require("admin-lte");
 
 // This file is automatically compiled by Webpack, along with any other files
@@ -11,13 +12,13 @@ require("admin-lte");
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+// import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import '../stylesheets/application'
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// import 'bootstrap'
+// import '../stylesheets/application'
+//
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
