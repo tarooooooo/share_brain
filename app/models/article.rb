387a7865e0ad5b20fcb92bd2056b08a1fb06ce6b
tarooooooo <ryutaromaeda83@gmail.com>
2022-02-25 07:14:56 +0900

    aasmによる状態遷移管理導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,36 @@
 class Article < ApplicationRecord
+  include AASM
+
+  aasm column: :publish_status, enum: true do
+    state :draft, initial: true
+    state :reserved
+    state :published
+    state :canceled
+
+    event :draft do
+      transitions from: :published, to: :draft
+    end
+
+    event :reserved do
+      transitions from: :draft, to: :reserved
+    end
+
+    event :published do
+      transitions from: [:draft, :reserved], to: :published
+    end
+
+    event :canceled do
+      transitions from: :published, to: :canceled
+    end
+  end
+
   belongs_to :writer, class_name:'User'
   enum publish_status: {
     draft: 0,
-    published: 10,
-    close: 99
-  }
+    reserved: 10,
+    published: 20,
+    canceled: 99
+  }, _prefix: true
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
