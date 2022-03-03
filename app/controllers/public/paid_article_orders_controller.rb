6d81fba1a81b9971f55a55ef06fb1c769e58db46
tarooooooo <ryutaromaeda83@gmail.com>
2022-03-04 07:27:06 +0900

    購入フロー実装（sale）

--- app/controllers/public/paid_article_orders_controller.rb
+++ app/controllers/public/paid_article_orders_controller.rb
@@ -11,12 +11,12 @@ class Public::PaidArticleOrdersController < ::Public::BaseController
   end
 
   def create
-    render :new and return if params[:back]
-    if @paid_article_order.save
-      redirect_to done_public_paid_article_paid_article_orders_path(paid_artcle_id: @paid_article.id)
-    else
-      render :new
-    end
+    render :new and return if params[:payment_method]
+    ::PaidArticleOrders::CreateWorkflow.run!(paid_article_order: @paid_article_order)
+    redirect_to done_public_paid_article_paid_article_orders_path(paid_article_id: @paid_article.id)
+
+  rescue StandardError => e
+    redirect_to public_paid_article_path, alert: e.message
   end
 
   def done; end
--- app/models/paid_article_sale.rb
+++ app/models/paid_article_sale.rb
@@ -2,14 +2,14 @@ class PaidArticleSale < ApplicationRecord
   belongs_to :paid_article_order
 
   class << self
-    def create_now(order)
-      paid_article = order.paid_article
+    def create_now(paid_article_order:)
+      paid_article = paid_article_order.paid_article
       seller = paid_article.seller
       fee_rate = 0.1
       fee = paid_article.price
       PaidArticleSale.create!(
         seller_id: seller.id,
-        paid_article_order: order,
+        paid_article_order: paid_article_order,
         fee_rate: fee_rate,
         fee: fee
       )
