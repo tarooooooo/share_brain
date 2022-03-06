3ac322471f1c6fc6d44d5c5a993fdee22ce4e806
ryutaro maeda <75834810+tarooooooo@users.noreply.github.com>
2022-03-06 11:55:28 +0900

    Revert "Paid article content form"

--- app/decorators/paid_article_content_decorator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module PaidArticleContentDecorator
-
-  def movie_iframe_url
-    return '' if movie_id.blank?
-    "https://www.youtube.com/embed/#{movie_id}?autoplay=1&mute=1&playsinline=1&loop=1&playlist=#{movie_id}"
-  end
-end
--- app/models/paid_article_content.rb
+++
@@ -1,4 +0,0 @@
-class PaidArticleContent < ApplicationRecord
-  mount_uploader :main_image, PaidArticleContentMainImageUploader
-  belongs_to :paid_article
-end
