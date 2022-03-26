f82f4e664a8021dcae87effaf8dd3477fb821b40
tarooooooo <ryutaromaeda83@gmail.com>
2022-03-26 13:40:31 +0900

    Production用のdeviseメールアドレス設定の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,19 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {:host => 'sharebrain-rtr.herokuapp.com', :protocol => 'http'}
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name: ENV['SEND_MAIL'],
-    password: ENV['GMAIL_SPECIFIC_PASSWORD'],
-    authentication: :login,
-    openssl_verify_mode: 'none',
-    enable_starttls_auto: true
-  }
+  config.hosts << "localhost.com"
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Rails.application.credentials.google[:mail],
+    :password => Rails.application.credentials.google[:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
