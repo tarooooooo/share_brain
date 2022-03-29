b5e69ad97293037ce1340e08c8d32405f13dc2cc
tarooooooo <ryutaromaeda83@gmail.com>
2022-03-30 08:05:20 +0900

    矢印を追加

--- app/javascript/packs/public/top_page/show.js
+++ app/javascript/packs/public/top_page/show.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', function(){
     $('.faq_list_question').on('click', function(){
+        $('.faq_list_question').not(this).removeClass('open');
+        $('.faq_list_question').not(this).next().slideUp();
         $(this).next().slideToggle();
+        $(this).toggleClass('open');
     });
 });
