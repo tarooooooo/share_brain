6b8bdc914cba9eeb3fcd33af0bfd6ac9534df552
tarooooooo <ryutaromaeda83@gmail.com>
2022-03-30 19:33:47 +0900

    モーダルを作成

--- app/javascript/packs/public/top_page/show.js
+++ app/javascript/packs/public/top_page/show.js
@@ -5,4 +5,13 @@ window.addEventListener('DOMContentLoaded', function(){
         $(this).next().slideToggle();
         $(this).toggleClass('open');
     });
+
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
 });
