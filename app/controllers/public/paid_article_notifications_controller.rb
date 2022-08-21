43cd319e51531197cb8e72098ce46d27e61dbf90
tarooooooo <ryutaromaeda83@gmail.com>
2022-08-21 13:34:00 +0900

    fix: order flow

---
+++ app/controllers/public/paid_article_notifications_controller.rb
@@ -0,0 +1,48 @@
+class Public::PaidArticleNotificationsController < ApplicationController
+  before_action :set_paid_article_notification, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @paid_article_notification = PaidArticleNotification.new
+    @paid_articles = current_user.paid_articles
+  end
+
+  def create
+    @paid_article_notification = PaidArticleNotification.new(paid_article_notification_params)
+    @paid_article_notification.writer = current_user
+    if @paid_article_notification.save!
+      redirect_to public_paid_article_notifications_path, notice: 'お知らせを作成しました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show; end
+
+  def index
+    @paid_article_notifications = current_user.paid_article_notifications
+  end
+
+  def destroy
+  end
+
+  private
+  def set_paid_article_notification
+    @paid_article_notification = PaidArticleNotification.find(params[:id])
+  end
+
+  def paid_article_notification_params
+    params.require(:paid_article_notification).permit(
+    :title,
+    :body,
+    :published_at,
+    :paid_article_id,
+    :writer_id
+    )
+  end
+end
--- app/controllers/public/paid_article_orders_controller.rb
+++ app/controllers/public/paid_article_orders_controller.rb
@@ -8,6 +8,7 @@ class Public::PaidArticleOrdersController < ::Public::BaseController
   def new; end
 
   def confirm
+    # byebug
     if @paid_article.present?
       @paid_article_order.payment_method = params[:paid_article_order][:payment_method]
       render :new unless @paid_article_order.payment_method.present?
@@ -19,13 +20,12 @@ class Public::PaidArticleOrdersController < ::Public::BaseController
 
   def create
     if @paid_article_order.present?
-      ::PaidArticleOrders::CreateWorkflow.run!(paid_article_order: @paid_article_order)
+      ::PaidArticleOrders::CreateWorkflow.run!(paid_article_order: @paid_article_order, paid_article: @paid_article)
       redirect_to public_paid_article_path(@paid_article)
     else
-      ::ArticleOrders::CreateWorkflow.run!(paid_article_order: @article_order)
+      ::ArticleOrders::CreateWorkflow.run!(paid_article_order: @article_order, article: @article)
       redirect_to public_article_path(@article)
     end
-
 
   rescue StandardError => e
     redirect_to public_paid_articles_path, alert: e.message
@@ -34,18 +34,28 @@ class Public::PaidArticleOrdersController < ::Public::BaseController
   private
   def set_paid_article
     @paid_article = PaidArticle.find(params[:paid_article_id]) if params[:type] == "paid_article"
+    @paid_article = PaidArticle.find(params[:paid_article_id]) if params[:paid_article_order].present? && params[:paid_article_order][:order_type] == "paid_article_order"
   end
 
   def set_article
-    @article = Article.find(params[:paid_article_id])
+    @article = Article.find(params[:paid_article_id]) if params[:type] == "article"
+    @article = Article.find(params[:paid_article_id]) if params[:article_order].present? && params[:article_order][:order_type] == "article_order"
   end
 
   def set_paid_article_order
-    @paid_article_order = current_user.paid_article_orders.new(paid_article: @paid_article) if params[:type] == "paid_article"
+    if params[:order_type] == "paid_article_order"
+      @paid_article_order = current_user.paid_article_orders.new(paid_article: @paid_article)
+    elsif params[:paid_article_order].present? && params[:paid_article_order][:order_type] == "paid_article_order"
+      @paid_article_order = current_user.paid_article_orders.new(paid_article: @paid_article)
+    end
   end
 
   def set_article_order
-    @article_order = current_user.paid_article_orders.new(article: @article)
+    if params[:order_type] == "article_order"
+      @article_order = current_user.paid_article_orders.new(article: @article)
+    elsif params[:article_order].present? && params[:article_order][:order_type] == "article_order"
+      @article_order = current_user.paid_article_orders.new(article: @article)
+    end
   end
 
 end
