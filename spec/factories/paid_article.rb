309eeb4dae53d4b44c53ff2dd14346883d2f0926
tarooooooo <ryutaromaeda83@gmail.com>
2022-08-29 20:03:07 +0900

    fix: factory.bot

--- spec/factories/paid_article.rb
+++ spec/factories/paid_article.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :paid_article do
     title { "title" }
     body { "body" }
-    seller { create(:user) }
+    association :seller, factory: :user
     price { 200 }
     published_at { Time.current }
   end
--- spec/factories/paid_article_notification.rb
+++ spec/factories/paid_article_notification.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :paid_article_notification do
     title { "title" }
     body { "body" }
-    writer { create(:user) }
-    paid_article { create(:paid_article) }
+    association :writer, factory: :user
+    association :paid_article, factory: :paid_article
     published_at { Time.current }
   end
 end
